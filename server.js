d72baa37bcb722e117a6beaf0ecae19f70617f13
Mark Murphy <mark@murphymark.me>
2017-08-06 14:09:06 -0500

    Connected to Mongo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const employersController = require('./controllers/employers.js');
 app.use ('/employers', employersController);
@@ -10,4 +11,10 @@ app.listen(3000, () => {
 
 app.get ('/', (req, res)=> {
   res.render('index.ejs');
+});
+
+mongoose.connect('mongodb://localhost:27017/jobboard');
+
+mongoose.connection.once('open', ()=> {
+  console.log('connected to Mongo');
 });
