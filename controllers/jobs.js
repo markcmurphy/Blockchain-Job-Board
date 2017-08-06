1af05eade2e595f6f672a6f6b306f2a579a55d2d
Mark Murphy <mark@murphymark.me>
2017-08-06 15:17:35 -0500

    created new jobs page

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/new', (req,res)=>{
+  res.render('jobs/new.ejs');
+});
+
 router.get('/', (req, res)=> {
   res.render('jobs/index.ejs')
 });
