5b389db274512e81cc2ccabea023897958d02a64
Mark Murphy <mark@murphymark.me>
2017-08-07 12:24:15 -0500

    user authentication and encryption working

--- server.js
+++ server.js
@@ -3,17 +3,26 @@ const app = express();
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/jobboard';
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
-const session = require('express-session');
-const usersController = require('./controllers/users.js');
-app.use('/users', usersController);
-const sessionsController = require('./controllers/sessions.js');
-app.use('/sessions', sessionsController);
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 
-
+const session = require('express-session');
+app.use(session({
+  secret: "oldShoeFarm",
+  resave: false,
+  saveUninitialized: false
+}));
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
+
+const usersController = require('./controllers/users.js');
+app.use('/users', usersController);
+
+const sessionsController = require('./controllers/session.js');
+app.use('/sessions', sessionsController);
+
+
+
 
 const employersController = require('./controllers/employers.js');
 app.use ('/employers', employersController);
@@ -21,24 +30,29 @@ app.use ('/employers', employersController);
 const jobsController = require('./controllers/jobs.js');
 app.use('/jobs', jobsController);
 
-app.use(session({
-  secret: "oldShoeFarm",
-  resave: false,
-  saveUninitialized: false
-}));
 
-app.listen(port, () => {
-  console.log('I am listening...');
+app.get('/app', function(req, res){
+    if(req.session.currentuser){
+        res.send('the party');
+    } else {
+        res.redirect('/sessions/new');
+    }
 });
 
-app.get('/', function(req, res){
+
+app.get('/', (req, res) => {
     res.render('index.ejs', {
         currentUser: req.session.currentuser
     });
 });
 
+
 mongoose.connect(mongoUri);
 
 mongoose.connection.once('open', ()=> {
   console.log('connected to Mongo');
+});
+
+app.listen(port, () => {
+  console.log('I am listening...');
 });
