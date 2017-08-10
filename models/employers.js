bdbe2fdf8c4f6f9300ae26c2b9e7b9616f5e23b0
Mark Murphy <mark@murphymark.me>
2017-08-10 12:07:32 -0500

    Added breadcrumbs, restyled create forms, restyled index pages for employers/jobs

--- models/employers.js
+++ models/employers.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Job = require('./jobs.js');
 const employerSchema = mongoose.Schema({
   name: String,
+  logo: String,
+  desc: String,
   jobs: [Job.schema]
 });
 
